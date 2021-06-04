444f7af3da63bfe94d48fd8fd84a19b49f756b56
eseruizgu <eseruizgu@unal.edu.co>
2021-06-04 08:46:16 -0500

    tarea ciclos

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -212,7 +212,8 @@ function esVocal(letra){
   //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra.length>1){return "Dato incorrecto"}else{if (letra= "a" || "e"||"i"||"o"||"u"){return "Es vocal"}}
+  if (letra.length>1){return "Dato incorrecto"}
+  else{if (letra=== "a"||"e"||"i"||"o"||"u"){return "Es vocal"}}
 }
 
 
